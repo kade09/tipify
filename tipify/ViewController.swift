6268cad7d7ec0cea8d0b922588e2a70d988b3a1e
kade09 <kade@ucsd.edu>
2020-07-20 14:50:01 -0700

    Initial Commit

---
+++ tipify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipify
+//
+//  Created by mac on 7/20/20.
+//  Copyright © 2020 KhalidIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
